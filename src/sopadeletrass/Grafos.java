28ca83bcdbf3839a1f75331349dd429bdea1078b
KelvinBecerra <urielbecerra39@gmail.com>
2024-06-07 17:13:27 -0400

    subida

--- src/sopadeletrass/Grafos.java
+++ src/sopadeletrass/Grafos.java
@@ -4,6 +4,9 @@
  */
 package sopadeletrass;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author KelvinCi
@@ -26,20 +29,19 @@ public class Grafos {
                 matriz_ady[i][j] = matriz_ady[j][i] = new Adyacencia(false);
             }
         }
-        letras = new NodoGrafo[max] ;
+        letras = new NodoGrafo[max];
     }
 
     public void insertar_vertices(char letra) {
         NodoGrafo nuevo = new NodoGrafo(num_vertices, letra);
         letras[num_vertices] = nuevo;
-        
-        
+
         if (num_vertices + 1 <= max_nodos) {
             for (int i = 0; i < num_vertices + 1; i++) {
                 for (int j = num_vertices; j < num_vertices + 1; j++) {
                     matriz_ady[i][j].es_adyacente = matriz_ady[j][i].es_adyacente = false;
-                    matriz_ady[i][j].destino = matriz_ady[j][i].origen= nuevo;
-                    matriz_ady[i][j].origen = matriz_ady[j][i].origen= letras[i];
+                    matriz_ady[i][j].destino = matriz_ady[j][i].origen = nuevo;
+                    matriz_ady[i][j].origen = matriz_ady[j][i].origen = letras[i];
                 }
             }
             num_vertices += 1;
@@ -107,14 +109,116 @@ public class Grafos {
     public void imprimirTabla() {
         System.out.println("La matriz contiene " + num_vertices + " vértices: \n");
         for (int i = 0; i < num_vertices; i++) {
+            System.out.print(this.letras[i].letra + "   ");
             for (int j = 0; j < num_vertices; j++) {
                 if (matriz_ady[i][j].es_adyacente) {
-                    System.out.print("1 ");
-                } else {
-                    System.out.print("0 ");
+                    System.out.print(this.letras[j].letra);
                 }
             }
             System.out.println("");
+        }
+    }
+
+
+    public NodoGrafo[] dfs(String palabra) {
+        boolean[] visited = new boolean[num_vertices];
+        for (int i = 0; i < 16; i++) {
+            visited[i] = false;
+        }
+        NodoGrafo[] found = new NodoGrafo[palabra.length()];
+        for (int i = 0; i < 16; i++) {
+            if (letras[i].letra == palabra.charAt(0)) {
+//                System.out.println("Inicio " + letras[i].letra);
+                found[0] = letras[i];
+                found = dfsRecursive(i, visited, palabra, found, 1);
+            }
+        }
+
+        return found;
+    }
+
+    private NodoGrafo[] dfsRecursive(int vertex, boolean[] visited, String palabra, NodoGrafo[] np, int pos) {
+        visited[vertex] = true;
+        if (pos == palabra.length()) {
+            return np;
+        }
+//        traversal.append(letras[vertex].letra);
+
+        for (int i = 0; i < num_vertices; i++) {
+            boolean l = this.letras[i].letra == palabra.charAt(pos);
+//            System.out.println(matriz_ady[vertex][i].es_adyacente + "  " + palabra.charAt(pos) + " y " + this.letras[i].letra + " = " + l);
+            if (matriz_ady[vertex][i].es_adyacente && !visited[i] && this.letras[i].letra == palabra.charAt(pos)) {
+                np[pos] = this.letras[i];
+//                System.out.println("Letra: " + this.letras[i].letra);
+                np = dfsRecursive(i, visited, palabra, np, pos + 1);
+            }
+        }
+        return np;
+    }
+
+    public AuxiliarBFS_1 bfsSearch(String word) {
+    AuxiliarBFS_1 aux = new AuxiliarBFS_1();
+    for (int i = 0; i < num_vertices; i++) {
+        if (bfsSearchFromVertex(i, word, aux)) {
+            return aux;
+        }
+    }
+    return aux;
+}
+
+private boolean bfsSearchFromVertex(int start, String word, AuxiliarBFS_1 aux) {
+    boolean[] visited = new boolean[num_vertices];
+    Queue<Integer> queue = new LinkedList<>();
+
+    queue.add(start);
+    visited[start] = true;
+
+    while (!queue.isEmpty()) {
+        int vertex = queue.poll();
+        if (bfsSearchFromVertexHelper(vertex, word, 0, visited, aux)) {
+            aux.encontrado = true;
+            return true;
+        }
+
+        for (int i = 0; i < num_vertices; i++) {
+            if (matriz_ady[vertex][i].es_adyacente && !visited[i]) {
+                queue.add(i);
+                visited[i] = true;
+            }
+        }
+    }
+
+    return false;
+}
+
+private boolean bfsSearchFromVertexHelper(int vertex, String word, int index, boolean[] visited, AuxiliarBFS_1 aux) {
+    if (index == word.length()) {
+        return true;
+    }
+
+    if (letras[vertex].letra != word.charAt(index)) {
+        return false;
+    }
+
+    aux.recorrido[index] = letras[vertex];
+
+    for (int i = 0; i < num_vertices; i++) {
+        if (matriz_ady[vertex][i].es_adyacente && !visited[i]) {
+            visited[i] = true;
+            if (bfsSearchFromVertexHelper(i, word, index + 1, visited, aux)) {
+                return true;
+            }
+            visited[i] = false;
+        }
+    }
+
+    return false;
+}
+
+    public void reboot(boolean[] visitados) {
+        for (int i = 0; i < this.num_vertices; i++) {
+            visitados[i] = false;
+
         }
     }
 }
