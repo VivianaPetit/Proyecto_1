ae1e8d3ffe8d9925218cb78153cc1a2f6330b979
KelvinBecerra <urielbecerra39@gmail.com>
2024-06-07 17:12:33 -0400

    fixing errors

---
+++ src/sopadeletrass/AuxiliarBFS_1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sopadeletrass;
+
+/**
+ *
+ * @author KelvinCi
+ */
+public class AuxiliarBFS_1 {
+     public NodoGrafo[] recorrido;
+    boolean encontrado;
+    
+    public AuxiliarBFS_1(){
+        recorrido = new NodoGrafo[16];
+        encontrado = false;
+    }
+}
